dbd77401dfc6e4e929521c78d38243fe333b0b44
senghuot <senghuotlim@gmail.com>
2015-08-12 17:38:00 -0700

    upload and zip

---
+++ client/js/controllers/filename-controller.js
@@ -0,0 +1,7 @@
+// extending demo module
+var demo = angular.module('demo', ['ngResource']);
+
+demo.controller('filenameController', ['$scope', function ($scope) {
+  $scope.filename = function() {
+  }
+}]);
--- server.js
+++ server.js
@@ -5,45 +5,74 @@ var express           = require('express'),
     mongoose          = require('mongoose'),
     fs                = require('fs'),
     Grid              = require('gridfs-stream'),
-    meetupController  = require('./server/controllers/meetups-controller'),
-    fileController    = require('./server/controllers/files-controller');
+    zlib              = require('zlib'),
+    gzip              = zlib.createGzip(),
+    gunzip            = zlib.createGunzip();
+
 
 // connect to server
 mongoose.connect('mongodb://localhost:27017/gridfs');
 var conn = mongoose.connection;
 
-// to save the data
-var buffer = '';
-
 Grid.mongo = mongoose.mongo;
 conn.once('open', function() {
   var gfs = Grid(conn.db);
 
-  // to remove a file  
-  // gfs.remove({_id: "55c8d8d2bbc3df0c3622ebf1"}, function(err) {
-  //   if (err) console.log(err);
-  //   else console.log('success');
-  // });
-
-  // // writing some files into db
-  // var writeStream = gfs.createWriteStream({
-  //   filename: '332.pdf'
-  // });
-  // fs.createReadStream('files/332.pdf').pipe(writeStream);
-  // writeStream.on('close', function() {console.log('finished.')})
-
-  // // write content to file system
-  var writeStream = fs.createWriteStream('332.pdf');
-
-  // read from mongodb
-  var readStream = gfs.createReadStream({filename: '332.pdf'});
-  readStream.pipe(writeStream);
-  writeStream.on('close', function() {
-    console.log('done');
-  });
-
+  //write(gfs, 'files/', 'oversize.pdf');
+  //read(gfs, 'oversize.pdf')
+  //remove(gfs, 'oversize.pdf')
+  //zip('files/', 'oversize.pdf');
+  //unzip('java.pdf');
 });
 
+// writing files onto mongodb
+function write(gfs, path, filename, start) {
+  // var filename = $('#filename').val().replace('C:\\fakepath\\', '');
+  var writeStream = gfs.createWriteStream({filename: filename});
+  fs.createReadStream(path + filename).pipe(writeStream);
+  writeStream.on('close', function() {
+    var end = new Date().getTime();
+    console.log(end - start);
+  });
+}
+
+// reading files onto mongodb
+function read(gfs, filename) {
+  var writeStream = fs.createWriteStream(filename);
+  var readStream  = gfs.createReadStream({filename: filename});
+  readStream.pipe(writeStream);
+  writeStream.on('close', function() {
+    console.log('finished reading ' + filename);
+  });
+}
+
+// removing files onto mongodb
+function remove(gfs, filename) {
+  gfs.remove({filename: filename}, function(err) {
+    if (err) console.log(err);
+    else console.log('finished deleting ' + filename);
+  });
+}
+
+function zip(path, filename) {
+  var start = new Date().getTime();
+  var readStream = fs.createReadStream(path + filename);
+  var writeStream = fs.createWriteStream(filename + '.gz');
+  readStream.pipe(gzip).pipe(writeStream);
+  writeStream.on('finish', function() {
+    console.log('finished compressing');
+    write(gfs, '', 'oversize.pdf.gz', start)
+  });
+}
+
+function unzip(filename) {
+  var readStream = fs.createReadStream(filename + '.gz');
+  var writeStream = fs.createWriteStream(filename);
+  readStream.pipe(gunzip).pipe(writeStream);
+  writeStream.on('finish', function() {
+    console.log('finished decompressing');
+  });
+}
 
 // this is where we map the folder for anuglar MVC
 app.use(bodyParser());
@@ -55,10 +84,6 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/client/views/index.html');
 });
-
-
-app.post('/api/meetups', meetupController.create);
-app.get('/api/meetups', meetupController.get);
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...');
